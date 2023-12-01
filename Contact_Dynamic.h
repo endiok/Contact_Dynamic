ff2b73b27c0c09aa0ca8695df666cd433884f0ad
endiok <137604306+endiok@users.noreply.github.com>
2023-12-01 08:17:11 +0800

    Create Contact_Dynamic.h

---
+++ Contact_Dynamic.h
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+enum Option//枚举增强代码可读性
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW,
+	SORT
+};
+#ifndef __CONTACT_H__
+#define __CONTACT_H__
+#define DEFAULT_SZ 3
+#define MAX_NAME 20
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 20
+typedef struct PeoInfo
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+}PeoInfo;
+
+typedef struct Contact
+{
+	struct PeoInfo* data;
+	int capacity;
+	int size;
+}Contact;
+
+
+void InitContact(struct Contact* ps);
+void AddContact(struct Contact* ps);
+void ShowContact(const struct Contact* ps);
+void DelContact(struct Contact* ps);
+void SearchContact(const struct Contact* ps);
+void ModifyContact(struct Contact* ps);
+void SortContact(struct Contact* ps);
+void DestoryContact(struct Contact* ps);
+
+#endif
